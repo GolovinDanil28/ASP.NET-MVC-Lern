5ed1c945b20a8d1c2dc594767f8d5514ab7644f1
GolovinDanil28 <golovindanil28@yandex.ru>
2022-11-18 23:33:08 +0500

    add model and conroller Product

--- WebApplication1/Data/ApplicationDbContext.cs
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -12,5 +12,7 @@ namespace WebApplication1.Data
         }
         public DbSet<Category> Category { get; set; }
         public DbSet<ApplicationType> ApplicationType { get; set; }
+
+        public DbSet<Product> Product { get; set; }
     }
 }
